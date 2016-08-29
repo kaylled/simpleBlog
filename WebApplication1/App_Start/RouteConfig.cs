de466a29a775f74404127ed696d0df2acf9965ee
DESKTOP-FKSHOTV\Khaled <kaylled@hotmail.com>
2016-08-29 11:07:21 +0200

    my first commit

---
+++ WebApplication1/App_Start/RouteConfig.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using WebApplication1.Controllers;
+
+namespace WebApplication1
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+
+            var namespaces = new[] { typeof(PostsController).Namespace };
+
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Posts",
+                url: "{controller}/{action}", defaults: new { controller = "Posts", action = "Index"
+                },
+                namespaces: namespaces
+            );
+
+            routes.MapRoute(
+                name:"Login", 
+                url: "{controller}/{action}", 
+                defaults: new {controller = "Auth", action = "Login"},
+                namespaces:namespaces);
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
